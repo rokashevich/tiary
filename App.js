45f06d16f0978de2f870dc944391cf7956dfd5a0
Александр Рокашевич <rokashevich@raa-st.ru>
2020-08-13 11:38:04 +0300

    Проброс иднтификатора нажатой кнопки в родительский класс

--- App.js
+++ App.js
@@ -1,96 +1,106 @@
-import * as React from 'react';
+import React, { Component } from 'react';
 import { TouchableOpacity, Text, View, Image, ScrollView } from 'react-native';
 import { Dimensions, PixelRatio } from 'react-native';
-import Svg, {  Path,} from 'react-native-svg';
+import Svg, { Path } from 'react-native-svg';
 
-import { Icons, Backetball, Car, School, Food, Cog } from './components/Icons'
+import { Icons, Backetball, Car, School, Food, Cog } from './components/Icons';
 
 // Всё дерево категорий хранится в единой структуре, т.к. он никогда не будет слишком большим в принципе.
 var nodes = [
   {
-    "title":"Спорт",
-    "description":"Упражнения на силу, выносливость и растяжка",
-    "icon":"dumbbells",
-    "entries":[
-      {"title":"Подтягивания"},
-      {"title":"Отжимания"},
-      {"title":"Бёрпи"},
-      {"title":"Лодочка"},
-      {"title":"Пресс"},
-      {"title":"Растяжка"}
-    ]
+    title: 'Спорт',
+    description: 'Упражнения на силу, выносливость и растяжка',
+    icon: 'dumbbells',
+    entries: [
+      { title: 'Подтягивания' },
+      { title: 'Отжимания' },
+      { title: 'Бёрпи' },
+      { title: 'Лодочка' },
+      { title: 'Пресс' },
+      { title: 'Растяжка' },
+    ],
   },
   {
-    "title":"Овощи",
-    "icon":"vegetables",
-    "entries":[
-      {"title":"Кабачок"},
-      {"title":"Свёкла"},
-      {"title":"Морковь"},
-      {"title":"Тыква"},
-    ]
+    title: 'Овощи',
+    icon: 'vegetables',
+    entries: [
+      { title: 'Кабачок' },
+      { title: 'Свёкла' },
+      { title: 'Морковь' },
+      { title: 'Тыква' },
+    ],
   },
   {
-    "title":"Завтраки",
-    "icon":"porridge",
-    "entries":[
-      {"title":"Рис с яйцом"},
-      {"title":"Греча"},
-      {"title":"Овсянка, сэр!"},
-      {"title":"Полба"},
-    ]
+    title: 'Завтраки',
+    icon: 'porridge',
+    entries: [
+      { title: 'Рис с яйцом' },
+      { title: 'Греча' },
+      { title: 'Овсянка, сэр!' },
+      { title: 'Полба' },
+    ],
   },
   {
-    "title":"Здоровье",
-    "icon":"porridge",
-    "entries":[
-      {"title":"Давление"},
-      {"title":"Пульс"}
-    ]
+    title: 'Здоровье',
+    icon: 'porridge',
+    entries: [{ title: 'Давление' }, { title: 'Пульс' }],
   },
   {
-    "title":"Авто",
-    "icon":"porridge",
-    "entries":[
-      {"title":"Топливо"},
-      {"title":"Прочее"}
-    ]
+    title: 'Авто',
+    icon: 'porridge',
+    entries: [{ title: 'Топливо' }, { title: 'Прочее' }],
+  },
+];
+
+//const ButtonsRow = (props) => {
+class ButtonsRow extends Component {
+  render() {
+    var tmp_buttons = [
+    { id: 1, name: 'Backetball', color: 'orange' },
+    { id: 2, name: 'Car', color: 'green' },
+    { id: 3,  name: 'Food', color: 'gray' },
+    { id: 4,  name: 'School', color: 'blue' },
+    { id: 5,  name: 'Hospital', color: 'red' },
+    { id: 6,  name: 'Cog', color: 'black' },
+  ];
+  return tmp_buttons.map((button) => (
+    <TouchableOpacity onPress={() => this.props.handler(button.id)}>
+      <Icons name={button.name} color={button.color} />
+    </TouchableOpacity>
+  ));
   }
-]
-
-const ButtonsRow = (props) => {
   
-  var tmp_buttons = [
-    {"name":"Backetball", "color":"orange"}, 
-    {"name":"Car", "color":"green"}, 
-    {"name":"Food", "color":"gray"}, 
-    {"name":"School", "color":"blue"}, 
-    {"name":"Hospital", "color":"red"},
-    {"name":"Cog", "color":"black"}
-  ]
-  return  (
-      tmp_buttons.map(button => (  
-        <TouchableOpacity 
-          onPress={()=>{}}
-        >
-          <Icons name={button.name} color={button.color}/>
-        </TouchableOpacity>
-      ))
-  )
 }
 
-export default function App() {
+class App extends Component {
 
-  return (
-    <View style={{flex: 1}}>
-      <ScrollView horizontal showsHorizontalScrollIndicator={false} style={{flexGrow: 0,backgroundColor: '#ffffff'}}>
-        <ButtonsRow/>
-      </ScrollView>
+  constructor(props) {
+    super(props)
+    this.handler = this.handler.bind(this)
+  }  
 
-      <ScrollView style={{flex: 1, backgroundColor: '#fafafa'}}>
-        <Text>width={Dimensions.get('window').width} ratio={PixelRatio.get()}</Text>
+  handler(id) {
+    console.log(id)
+  }
 
-      </ScrollView>
-    </View>
-  );
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={{ flexGrow: 0, backgroundColor: '#ffffff' }}>
+          <ButtonsRow handler = {this.handler} />
+        </ScrollView>
+
+        <ScrollView style={{ flex: 1, backgroundColor: '#fafafa' }}>
+          <Text>
+            width={Dimensions.get('window').width} ratio={PixelRatio.get()}
+          </Text>
+        </ScrollView>
+      </View>
+    );
+  }
 }
+
+export default App;
