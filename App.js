d95893c39359d857e14c877249ae1efe0820cd25
Aleksandr Rokashevich <rokashevich@gmail.com>
2020-08-17 16:11:25 +0300

    Перевод классов в функции

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { TouchableOpacity, Text, View, Image, ScrollView } from 'react-native';
 import { Dimensions, PixelRatio } from 'react-native';
 import Svg, { Path } from 'react-native-svg';
@@ -53,84 +53,54 @@ var nodes = [
 ];
 
 var db = {
-11:"Basketball",
-12:"Car",
-13:"Food",
-14:"School",
-15:"Hospital"
-}
+  11: 'Basketball',
+  12: 'Car',
+  13: 'Food',
+  14: 'School',
+  15: 'Hospital',
+};
 
-//const ButtonsRow = (props) => {
-class ButtonsRow extends Component {
-  render() {
-    var tmp_buttons = [
+const ButtonsRow = (props) => {
+  var tmp_buttons = [
     { id: 11, name: 'Backetball', color: 'orange' },
     { id: 12, name: 'Car', color: 'green' },
-    { id: 13,  name: 'Food', color: 'gray' },
-    { id: 14,  name: 'School', color: 'blue' },
-    { id: 15,  name: 'Hospital', color: 'red' },
-    { id: 0,  name: 'Cog', color: 'black' },
+    { id: 13, name: 'Food', color: 'gray' },
+    { id: 14, name: 'School', color: 'blue' },
+    { id: 15, name: 'Hospital', color: 'red' },
+    { id: 0, name: 'Cog', color: 'black' },
   ];
-  console.log(this.props.size)
   return tmp_buttons.map((button) => (
-    <TouchableOpacity onPress={() => this.props.handler(button.id)}>
-      <Icons size={this.props.size} name={button.name} color={button.color}/>
+    <TouchableOpacity 
+      onPress={() => {
+        console.log(button.id)
+        props.handler({ node: button.id })
+      }}>
+      <Icons name={button.name} color={button.color} />
     </TouchableOpacity>
-  ));
-  }
-  
+  ))
 }
 
-class App extends Component {
-  state = {
-    node: 10,
-    // iPad 1024 2
-    // Xiaomi A1 360 3
-    // iPhone 375 2
-    // Android 360 3
-    // Xiaomi Redmi 360 3
-    base_size: 48,
-    max_columns: 0,
-    column_width: 0
-  }
+const App = () => {
+  const [node, setNode] = useState({ node: 10 });
+  return (
+    <View style={{ flex: 1 }}>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={{ flexGrow: 0, backgroundColor: '#ffffff' }}>
+        <ButtonsRow handler={setNode}/>
+      </ScrollView>
 
-  constructor(props) {
-    super(props)
-    this.handler = this.handler.bind(this)
-    this.state.max_columns = Math.floor(
-      Dimensions.get('window').width / this.state.base_size)
-    this.state.column_width = this.state.base_size + Math.floor(
-      ( Dimensions.get('window').width - this.state.max_columns*this.state.base_size
-      ) / this.state.max_columns)
-  }
-
-  handler(id) {
-    console.log(id,this.node)
-    this.setState({node:id})
-  }
-
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          style={{ flexGrow: 0, backgroundColor: '#ffffff' }}>
-          <ButtonsRow size={this.state.column_width} handler={this.handler} />
-        </ScrollView>
-
-        <ScrollView style={{ flex: 1, backgroundColor: '#fafafa' }}>
-          <Text>node={this.state.node}</Text>
-          <Text>base_size={this.state.base_size}</Text>
-          <Text>max_columns={this.state.max_columns}</Text>
-          <Text>column_width={this.state.column_width}</Text>
-          <Text>
-            width={Dimensions.get('window').width} ratio={PixelRatio.get()}
-          </Text>
-        </ScrollView>
-      </View>
-    );
-  }
+      <ScrollView
+        style={{ flex: 1, backgroundColor: '#fafafa' }}
+        stickyHeaderIndices={[0]}>
+        <Text>{node.node}</Text>
+        <Text>
+          width={Dimensions.get('window').width} ratio={PixelRatio.get()}
+        </Text>
+      </ScrollView>
+    </View>
+  )
 }
 
 export default App;
