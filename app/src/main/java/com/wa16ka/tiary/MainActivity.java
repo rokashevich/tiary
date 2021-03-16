d15a048ce94c2f427648070a0694ae13f1fabe77
Aleksandr Rokashevich <rokashevich@gmail.com>
2021-03-16 16:15:19 +0300

    https://developer.android.com/codelabs/android-training-layout-editor-part-a?index=..%2F..%2Fandroid-training#7

--- app/src/main/java/com/wa16ka/tiary/MainActivity.java
+++ app/src/main/java/com/wa16ka/tiary/MainActivity.java
@@ -2,11 +2,15 @@ package com.wa16ka.tiary;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private int mCount = 0;
+    private TextView mShowCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +20,16 @@ public class MainActivity extends AppCompatActivity {
         } catch (NullPointerException e) {
         }
         Log.d("MainActivity", "Hello World");
+        mShowCount = (TextView)findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount!=null)mShowCount.setText(Integer.toString(mCount));
     }
 }
